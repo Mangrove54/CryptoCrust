51848ef62d2400eb10195c4f2214b19414bc68d2
Mangrove54 <110115408+Mangrove54@users.noreply.github.com>
2023-04-26 11:49:02 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,36 @@
+export const menuArray = [
+    {
+        name: "Margherbita",
+        picture:"images/2.jpg",
+        ingredients: ["basil", "mozzarella","tomato"],
+        description: "Savor the simple yet refined flavors of a classic Margherita pizza, with vibrant San Marzano tomatoes, creamy fresh mozzarella, and fragrant basil, all atop a perfectly charred and chewy crust,making you relish every bit.",
+        price: 11,
+        id: 5410
+        
+    },
+    {
+        name: "Cardabbiata",
+        picture:"images/1.jpg",
+        ingredients: ["pancetta","black olives","mozzarella"],
+        description: `The Cardabbiata pizza is a mouth-watering masterpiece that combines the perfect amount of heat with fresh, tangy tomato sauce. Topped with melted mozzarella cheese,aged pancetta and black olives,every bite is an explosion of flavor that will leave you wanting more. A "DA"licious decision indeed.`,
+        price: 12,
+        id: 5411
+    },
+        {
+        name: "Quattripplo",
+        picture:"images/3.jpg",
+        ingredients: ["mozzarella","gorgonzola","parmesan","robiola","basil"],
+        description: "Indulge in the decadent flavors of the Quattro Formaggio pizza, featuring a delectable blend of four cheeses: gooey mozzarella, tangy Gorgonzola, savory Parmesan, and creamy Robiola. Each bite is a heavenly experience that will transport you to cheese lover's paradise,bringing ripples in your mouth with every bite.",
+        price: 15,
+        id: 5412
+    },
+    {
+        name: "Stellaroni",
+        picture:"images/4.jpg",
+        ingredients: ["mozzarella","pepperoni"],
+        description: "Satisfy your cravings with the classic Stellaroni pizza, loaded with savory slices of spicy pepperoni on a bed of melted mozzarella cheese and tangy tomato sauce. Each bite is a stellar balance of salty, spicy, and savory flavors that will leave you feeling satisfied and happy.",
+        price: 15,
+        id: 5413
+    }
+    
+]
---
+++ index.js
@@ -0,0 +1,137 @@
+import { menuArray } from '/data.js'// javascript
+const menu=document.getElementById("menu")
+const complete=document.getElementById("comp-order")
+const modal=document.getElementById("modal")
+let mybutton = document.getElementById("myBtn");
+const orderCont=document.getElementById("order-box")
+const order = document.getElementById("order");
+let addedOrderItems = [];
+function addMenu()
+{
+    let menuHtml=``
+    menuArray.forEach(function(menu)
+    {
+        menuHtml+=`
+        <div class="menu-container"> 
+        <img class="item-img" src="${menu.picture}"/> 
+                    <div class="menu-item-description">
+                        <div class="menu-item-name"> ${menu.name} </div>
+                        <div class="menu-item-ingredients"> [${menu.ingredients}] </div>
+                        <div class="menu-item-description"> ${menu.description} </div>
+                        <div class="menu-item-price"> $${menu.price} </div>  
+                    </div> 
+                    <div class="button-con">
+                    <button class="item-add" data-add-id="add-${menu.id}"><img class="add-img" src="images/add.png"/></button>
+                    <button class="item-remove" data-remove-id="remove-${menu.id}" disabled><img class="remove-img" src="images/remove.png"/></button>
+                    </div>
+                    
+        </div>
+        
+            <div class="menu-item-container-divider"> </div>`
+        ;
+  });
+  return menuHtml;
+}
+menu.innerHTML = addMenu();
+mybutton.addEventListener("click",topFunction)
+function topFunction() {
+  if (document.body.scrollTop !== undefined) {
+    document.body.scrollTop = 0; }
+  if (document.documentElement.scrollTop !== undefined) {
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+}
+function getOrderHtml() {
+  const orderList = document.getElementById("order-box");
+  let orderHtml = ``;
+
+  addedOrderItems.forEach(function (order) {
+    orderHtml += `
+                        <div class="menu-item-description order-item">   
+                            <div class="menu-item-name order-name"> ${order.name} </div>
+                            <div class="menu-item-name remove" data-remove-id="${order.id}"> remove </div>
+                            <div class="menu-item-price order-price">$${order.price}</div>
+                        </div>
+                    `;
+  });
+
+  orderList.innerHTML = orderHtml;
+  getOrderTotalPrice();
+  hideOrder();
+}
+
+document.addEventListener("click",function(e)
+{
+    
+    if(e.target.dataset.addId)
+    {
+        handleAddItem(e.target.dataset.addId)
+    }
+    else if(e.target.dataset.removeId)
+    {
+        removeOrderItem(e.target.dataset.removeId)
+    }
+    else if(e.target.dataset.complete)
+    {
+        modal.classList.toggle("hidden");
+    }
+   
+})
+
+function handleAddItem(menuId) {
+  const targetMenuObj = menuArray.filter(function (menu) {
+    return Number(menu.id) === Number(menuId);
+  })[0];
+
+  addedOrderItems.push(targetMenuObj);
+  
+  getOrderHtml();
+}
+
+function removeOrderItem(orderItemId) {
+  for (let i = 0; i < addedOrderItems.length; i++) {
+    if (Number(addedOrderItems[i].id) === Number(orderItemId)) {
+      addedOrderItems.splice(i, 1);
+      break;
+    }
+  }
+  getOrderHtml();
+}
+function hideOrder() {
+  if (addedOrderItems.length > 0) {
+    if (order.classList.contains("hidden")) order.classList.remove("hidden");
+  } else {
+    order.classList.add("hidden");
+  }
+}
+
+function getOrderTotalPrice() {
+  const totalPrice = addedOrderItems.reduce((sum, item) => sum + item.price, 0);
+  const totalElement = document.getElementById("total");
+  totalElement.innerHTML = totalPrice;
+}
+
+const paymentForm = document.getElementById("payment-form");
+const thankYou = document.getElementById("thank-you");
+
+paymentForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const paymentFormData = new FormData(paymentForm);
+  const name = paymentFormData.get("fullName");
+  thankYou.innerHTML = `Thanks, ${name} Your order is on its way! `;
+
+  removeOrderPayThankYou();
+});
+
+function removeOrderPayThankYou() {
+  order.classList.toggle("hidden");
+  thankYou.classList.remove("hidden");
+  modal.classList.toggle("hidden");
+  addedOrderItems.length = 0;
+  paymentForm.reset();
+  const myTimeout = setTimeout(removeThankYou, 5000);
+}
+
+function removeThankYou() {
+  thankYou.classList.add("hidden");
+}
